b73c8c6c42e52f940fa4b10b88b2fa59c0c489f6
omelenko <vasul.melenko@gmail.com>
2023-10-19 18:35:17 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        Thread thread = new Thread(new ThreadStart(Worker));
+        thread.Start();
+        thread.Join();
+    }
+
+    static void Worker()
+    {
+        string[] a = {"a","b","c","d","e","f"};
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(i);
+            Thread.Sleep(500);
+        }
+        foreach(string h in a)
+        {
+            Console.WriteLine(h);
+            Thread.Sleep(500);
+        }
+    }
+}
